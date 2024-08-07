c4c36dcd684773ed6c38e33c9315af54ff3ff7fc
sainianujsaini <anujsaini160@gmail.com>
2024-08-07 15:22:57 +0530

    before testing login and register page from backend created public folder and added login and register files

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
+import {dirname} from 'path';
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
 app.get("/" , (req , res)=>{
-    res.render("home.ejs");
+    res.sendFile(__dirname + "/ public/html/login.html");
 })
 app.listen(port,()=>{
     console.log("Server is up and running on port " + port);

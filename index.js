48d99cf852b3b7651b5097accf1904f1cc830b86
sainianujsaini <anujsaini160@gmail.com>
2024-08-26 13:03:17 +0530

    Commit it now yes

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ import session from "express-session";
 import passport from "passport";
 import {Strategy} from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
-import env from 'dotenv'
+import env from 'dotenv';
 const __dirname = dirname(fileURLToPath(import.meta.url));
+env.config();
 const app = express();
 const port = 4000;
 
@@ -424,6 +425,7 @@ passport.serializeUser((user , callback)=>{
 app.listen(port,()=>{
     console.log("Server is up and running on port " + port);
 });
+
 
 
 
